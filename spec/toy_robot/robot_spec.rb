0ad69c0224c5d1476db896d7e898c8806f606e09
Lee Sheppard <lee@leesheppard.com>
2017-06-24 00:59:30 +1000

    Initial test to move east and west

---
+++ spec/toy_robot/robot_spec.rb
@@ -0,0 +1,25 @@
+require "spec_helper"
+
+describe ToyRobot::Robot do
+  subject { ToyRobot::Robot.new(0) }
+
+  it "moves 3 spaces east" do
+    3.times { subject.move_east }
+    expect(subject.position).to eq(3)
+  end
+
+  it "moves 4 spaces east" do
+    4.times { subject.move_east }
+    expect(subject.position).to eq(4)
+  end
+
+  it "moves 3 spaces west" do
+    3.times { subject.move_west }
+    expect(subject.position).to eq(-3)
+  end
+
+  it "moves 3 spaces west" do
+    4.times { subject.move_west }
+    expect(subject.position).to eq(-4)
+  end
+end
