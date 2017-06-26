179b574a411270eb168511b69f8b0e813671c878
Lee Sheppard <lee@leesheppard.com>
2017-06-26 11:46:16 +1000

    Robot can now turn left and right

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,10 +1,12 @@
 module ToyRobot
   class Robot
-    attr_reader :east, :north
+    attr_reader :east, :north, :direction
+    DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
 
-    def initialize(east = 0, north = 0)
+    def initialize(east = 0, north = 0, direction = "NORTH")
       @east = east
       @north = north
+      @direction = direction
     end
 
     def move_east
@@ -23,8 +25,31 @@ module ToyRobot
       @north -= 1
     end
 
+    def move
+      send("move_#{@direction.downcase}")
+    end
+
+    def turn_left
+      # turn(:left)
+      @direction = DIRECTIONS[DIRECTIONS.index(@direction) - 1]
+    end
+
+    def turn_right
+      # turn(:right)
+      index = DIRECTIONS.index(@direction)
+      @direction = DIRECTIONS.rotate(1)[index]
+    end
+
     def position
       @position
     end
+  end
+
+  private
+
+  def turn(turn_direction)
+    index = DIRECTIONS.index(@direction)
+    rotations = turn_diretion == :right ? 1 : -1
+    @direction = DIRECTIONS.rotate(rotations)[index]
   end
 end
