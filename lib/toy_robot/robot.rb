664e3dce5fa9ec977f2b848e5d64a0667451e42d
Lee Sheppard <lee@leesheppard.com>
2024-06-09 12:49:26 +1000

    Add REPORT, PLACE and COMMAND

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,6 +1,6 @@
 module ToyRobot
   class Robot
-    DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
+    DIRECTIONS = %w[NORTH EAST SOUTH WEST].freeze
     attr_reader :east, :north, :direction
 
     def initialize(east = 0, north = 0, direction = "NORTH")
@@ -31,11 +31,25 @@ module ToyRobot
       send("move_#{@direction.downcase}")
     end
 
+    def next_move
+      case @direction
+      when "NORTH"
+        [@east, @north + 1]
+      when "SOUTH"
+        [@east, @north - 1]
+      when "EAST"
+        [@east + 1, @north]
+      when "WEST"
+        [@east - 1, @north]
+      end
+    end
+
     # Turn the robot
 
     def turn_right
       turn(:right)
     end
+
     def turn_left
       turn(:left)
     end
@@ -44,18 +58,18 @@ module ToyRobot
 
     def report
       {
-        north: north,
-        east: east,
-        direction:  direction
+        north: @north,
+        east: @east,
+        direction: @direction
       }
     end
 
     private
+
     def turn(turn_direction)
       index = DIRECTIONS.index(@direction)
       rotations = turn_direction == :right ? 1 : -1
       @direction = DIRECTIONS.rotate(rotations)[index]
     end
-
   end
 end
--- lib/toy_robot/simulator.rb
+++ lib/toy_robot/simulator.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 module ToyRobot
   class Simulator
     attr_reader :robot
@@ -10,6 +11,38 @@ module ToyRobot
       return unless @table.valid_location?(east, north)
 
       @robot = Robot.new(east, north, facing)
+    end
+
+    def move
+      return unless robot_placed?
+      return unless @table.valid_location?(*robot.next_move)
+
+      robot.move
+    end
+
+    def turn_left
+      return unless robot_placed?
+      robot.turn_left
+    end
+
+    def turn_right
+      return unless robot_placed?
+      robot.turn_right
+    end
+
+    def report
+      return unless robot_placed?
+      position = robot.report
+      puts "Robot is currently at (#{position[:east]}, #{position[:north]})" +
+        " and it's facing #{position[:direction]}"
+    end
+
+    def invalid(command)
+      puts "'#{command.strip}' is an invalid command"
+    end
+
+    def robot_placed?
+      !robot.nil?
     end
   end
 end
